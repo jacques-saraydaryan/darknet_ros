48f39ca75f917a2a592d05505ad20688d2cfabbd
jsaraydaryan <j@j.com>
2021-03-26 16:40:02 +0100

    apply modification to allow multiple image flow of different size

--- darknet_ros/include/darknet_ros/YoloObjectDetector.hpp
+++ darknet_ros/include/darknet_ros/YoloObjectDetector.hpp
@@ -9,72 +9,75 @@
 #pragma once
 
 // c++
-#include <pthread.h>
-#include <chrono>
-#include <cmath>
-#include <iostream>
+#include <math.h>
 #include <string>
-#include <thread>
 #include <vector>
+#include <iostream>
+#include <pthread.h>
+#include <thread>
+#include <chrono>
 
 // ROS
+#include <ros/ros.h>
+#include <std_msgs/Header.h>
 #include <actionlib/server/simple_action_server.h>
+#include <sensor_msgs/image_encodings.h>
+#include <sensor_msgs/Image.h>
 #include <geometry_msgs/Point.h>
 #include <image_transport/image_transport.h>
-#include <ros/ros.h>
-#include <sensor_msgs/Image.h>
-#include <sensor_msgs/image_encodings.h>
-#include <std_msgs/Header.h>
 
 // OpenCv
-#include <cv_bridge/cv_bridge.h>
-#include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <opencv2/objdetect/objdetect.hpp>
+#include <cv_bridge/cv_bridge.h>
 
 // darknet_ros_msgs
-#include <darknet_ros_msgs/BoundingBox.h>
 #include <darknet_ros_msgs/BoundingBoxes.h>
-#include <darknet_ros_msgs/CheckForObjectsAction.h>
+#include <darknet_ros_msgs/BoundingBox.h>
 #include <darknet_ros_msgs/ObjectCount.h>
+#include <darknet_ros_msgs/CheckForObjectsAction.h>
 
 // Darknet.
 #ifdef GPU
-#include "cublas_v2.h"
 #include "cuda_runtime.h"
 #include "curand.h"
+#include "cublas_v2.h"
 #endif
 
 extern "C" {
-#include <sys/time.h>
-#include "box.h"
-#include "cost_layer.h"
-#include "darknet_ros/image_interface.h"
-#include "detection_layer.h"
 #include "network.h"
-#include "parser.h"
+#include "detection_layer.h"
 #include "region_layer.h"
+#include "cost_layer.h"
 #include "utils.h"
+#include "parser.h"
+#include "box.h"
+#include "darknet_ros/image_interface.h"
+#include <sys/time.h>
 }
 
 extern "C" void ipl_into_image(IplImage* src, image im);
 extern "C" image ipl_to_image(IplImage* src);
-extern "C" void show_image_cv(image p, const char* name, IplImage* disp);
+extern "C" void show_image_cv(image p, const char *name, IplImage *disp);
 
 namespace darknet_ros {
 
 //! Bounding box of the detected object.
-typedef struct {
+typedef struct
+{
   float x, y, w, h, prob;
   int num, Class;
 } RosBox_;
 
-typedef struct {
+typedef struct
+{
   IplImage* image;
   std_msgs::Header header;
 } IplImageWithHeader_;
 
-class YoloObjectDetector {
+class YoloObjectDetector
+{
  public:
   /*!
    * Constructor.
@@ -126,9 +129,9 @@ class YoloObjectDetector {
    */
   bool publishDetectionImage(const cv::Mat& detectionImage);
 
-  //! Using.
-  using CheckForObjectsActionServer = actionlib::SimpleActionServer<darknet_ros_msgs::CheckForObjectsAction>;
-  using CheckForObjectsActionServerPtr = std::shared_ptr<CheckForObjectsActionServer>;
+  //! Typedefs.
+  typedef actionlib::SimpleActionServer<darknet_ros_msgs::CheckForObjectsAction> CheckForObjectsActionServer;
+  typedef std::shared_ptr<CheckForObjectsActionServer> CheckForObjectsActionServerPtr;
 
   //! ROS node handle.
   ros::NodeHandle nodeHandle_;
@@ -164,17 +167,23 @@ class YoloObjectDetector {
   std::thread yoloThread_;
 
   // Darknet.
-  char** demoNames_;
-  image** demoAlphabet_;
+  char **demoNames_;
+  image **demoAlphabet_;
   int demoClasses_;
 
-  network* net_;
+  network *net_;
   std_msgs::Header headerBuff_[3];
+  boost::shared_mutex mutexHeaderBuff_;
   image buff_[3];
+  boost::shared_mutex mutexBuff_;
   image buffLetter_[3];
+  boost::shared_mutex mutexBuffLetter_;
   int buffId_[3];
   int buffIndex_ = 0;
-  IplImage* ipl_;
+  boost::shared_mutex mutexBuffIndex_;
+  IplImage * ipl_;
+  boost::shared_mutex mutexIpl_;
+  std::string frameId;
   float fps_ = 0;
   float demoThresh_ = 0;
   float demoHier_ = .5;
@@ -182,21 +191,21 @@ class YoloObjectDetector {
 
   int demoDelay_ = 0;
   int demoFrame_ = 3;
-  float** predictions_;
+  float **predictions_;
   int demoIndex_ = 0;
   int demoDone_ = 0;
-  float* lastAvg2_;
-  float* lastAvg_;
-  float* avg_;
+  float *lastAvg2_;
+  float *lastAvg_;
+  float *avg_;
   int demoTotal_ = 0;
   double demoTime_;
 
-  RosBox_* roiBoxes_;
+  RosBox_ *roiBoxes_;
   bool viewImage_;
   bool enableConsoleOutput_;
   int waitKeyDelay_;
   int fullScreen_;
-  char* demoPrefix_;
+  char *demoPrefix_;
 
   std_msgs::Header imageHeader_;
   cv::Mat camImageCopy_;
@@ -211,26 +220,30 @@ class YoloObjectDetector {
   int actionId_;
   boost::shared_mutex mutexActionStatus_;
 
+   std::vector<image> img_to_free_vect;
+
   // double getWallTime();
 
-  int sizeNetwork(network* net);
+  int sizeNetwork(network *net);
 
-  void rememberNetwork(network* net);
+  void rememberNetwork(network *net);
 
-  detection* avgPredictions(network* net, int* nboxes);
+  detection *avgPredictions(network *net, int *nboxes);
 
-  void* detectInThread();
+  void *detectInThread();
 
-  void* fetchInThread();
+  void *fetchInThread();
 
-  void* displayInThread(void* ptr);
+  void *displayInThread(void *ptr);
 
-  void* displayLoop(void* ptr);
+  void *displayLoop(void *ptr);
 
-  void* detectLoop(void* ptr);
+  void *detectLoop(void *ptr);
 
-  void setupNetwork(char* cfgfile, char* weightfile, char* datafile, float thresh, char** names, int classes, int delay, char* prefix,
-                    int avg_frames, float hier, int w, int h, int frames, int fullscreen);
+  void setupNetwork(char *cfgfile, char *weightfile, char *datafile, float thresh,
+                    char **names, int classes,
+                    int delay, char *prefix, int avg_frames, float hier, int w, int h,
+                    int frames, int fullscreen);
 
   void yolo();
 
@@ -240,7 +253,26 @@ class YoloObjectDetector {
 
   bool isNodeRunning(void);
 
-  void* publishInThread();
+  void *publishInThread();
+
+  IplImage *getIpl_();
+
+  void setIpl_(IplImage *ipl);
+
+  int getBuffIndex_();
+
+  void setBuffIndex_(int index);
+
+  image getBuff_(int index);
+  void setBuff_(int index,image img);
+
+  std_msgs::Header getHeaderBuff(int index);
+  void setHeaderBuff(int index, std_msgs::Header head);
+
+  image getBuffLetter_(int index);
+  void setBuffLetter_(int index, image img);
+
+  void replaceImageBuff(int index, image img);
 };
 
 } /* namespace darknet_ros*/
